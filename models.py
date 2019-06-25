cab5b757045fa0b6f9b79c5408de952162757caf
root <root@localhost.localdomain>
2019-06-24 22:19:45 -0300

    last updates done at Mon 24 Jun 2019 10:19:45 PM -03

--- models.py
+++ models.py
@@ -2,11 +2,13 @@ import sqlite3 as sql
 from os import path
 
 ROOT = path.dirname(path.relpath((__file__)))
+con = sql.connect(path.join(ROOT, 'livraria.db'))
+cur = con.cursor()
 
 #adicionar funcionalidades
 def register(login, nome, email, senha, cpf, endereco):
-    con = sql.connect(path.join(ROOT, 'livraria.db'))
-    cur = con.cursor()
-    cur.execute('insert into cliente (login, nome, email, senha, cpf, endereco) values (?, ?, ?, ?, ?, ?)', (login, nome, email, senha, cpf, endereco))
+    with con:
+        cur.execute("INSERT INTO cliente (login, nome, email, senha, cpf, endereco) values (?, ?, ?, ?, ?, ?)", 
+                     (login, nome, email, senha, cpf, endereco))
     con.commit()
     con.close()
