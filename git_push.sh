38adcddbdece6d64398df83cca5d79d82a6bdff2
root <root@localhost.localdomain>
2019-06-26 00:50:58 -0300

    update para usar gitignore

--- git_push.sh
+++ git_push.sh
@@ -5,5 +5,5 @@ find . -type f -name ._\* -exec rm -f {} \;
 git add .
 git ls-files --others --exclude-from=.git/info/exclude
 TIME=`date`
-git commit -am "last updates done at $TIME"
+git commit -am "last updates done at $TIME" .gitignore
 git push
